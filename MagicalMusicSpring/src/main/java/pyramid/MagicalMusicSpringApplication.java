c17c7a488a13f7d9cd7ede3aac8aa86a1e0a0ac9
Unknown <will3shu@gmail.com>
2019-12-30 11:35:47 -0500

    Springboot Init

---
+++ MagicalMusicSpring/src/main/java/pyramid/MagicalMusicSpringApplication.java
@@ -0,0 +1,13 @@
+package pyramid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicalMusicSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicalMusicSpringApplication.class, args);
+	}
+
+}
