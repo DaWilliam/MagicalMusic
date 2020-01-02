8ce486b56b053c6dcbe36a57f0c19d2766cabeec
Unknown <will3shu@gmail.com>
2020-01-02 11:35:48 -0500

    Json Serialized "opensearch"

--- MagicalMusicSpring/src/main/java/pyramid/models/searchdata/Results.java
+++ MagicalMusicSpring/src/main/java/pyramid/models/searchdata/Results.java
@@ -1,11 +1,18 @@
 package pyramid.models.searchdata;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Results {
 	
+	@SerializedName("opensearch:Query")
 	public Query query;
+	@SerializedName("opensearch:totalResults")
 	public int totalResults;
+	@SerializedName("opensearch:startIndex")
 	public int startIndex;
+	@SerializedName("opensearch:itemsPerPage")
 	public int itemsPerPage;
+	
 	public TrackMatches trackmatches;
 	
 	public String statusCode;
